42781b27ab6c835860060176083e26657d107838
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2025-05-14 00:02:27 +0300

    pushed

--- output.asm
+++ output.asm
@@ -7,6 +7,25 @@ global _start
 
 _start:
     mov dword [x], 5
+    mov dword [y], 5
+    mov dword [z], 0
+    mov dword [t], false
+    mov dword [c], true
+    mov dword [m], "mimo"
+    mov dword [m], "zozi"
+    mov dword [x], 0
+    mov dword [y], 0
+    mov dword [z], 0
+    mov dword [i], 0
+    mov dword [x], 5
+    mov dword [y], 6
+    mov dword [x], 5
+    mov dword [y], 6
+    mov dword [x], 5
+    mov dword [y], 6
+    mov dword [x], 5
+    mov dword [y], 6
+    mov dword [z], 7
     mov eax, 1
     xor ebx, ebx
     int 0x80
