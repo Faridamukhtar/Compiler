9a0deb9834b30616c8fcbb717d2a527daadfe387
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2025-05-14 01:20:51 +0300

    quads done

--- output.asm
+++ output.asm
@@ -1,31 +1,20 @@
-section .data
-    x dd 0
-    y dd 0
-
-section .text
-global _start
-
-_start:
-    mov dword [x], 5
-    mov dword [y], 5
-    mov dword [z], 0
-    mov dword [t], false
-    mov dword [c], true
-    mov dword [m], "mimo"
-    mov dword [m], "zozi"
-    mov dword [x], 0
-    mov dword [y], 0
-    mov dword [z], 0
-    mov dword [i], 0
-    mov dword [x], 5
-    mov dword [y], 6
-    mov dword [x], 5
-    mov dword [y], 6
-    mov dword [x], 5
-    mov dword [y], 6
-    mov dword [x], 5
-    mov dword [y], 6
-    mov dword [z], 7
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+;
+mov x, 5
+mov y, 5
+eq t1, x, y
+jif t1, L1
+jmp L2
+L1:
+mov x, 0
+jmp L3
+L2:
+mov y, 0
+neq t2, x, y
+jif t2, L4
+jmp L5
+L4:
+mov z, 0
+jmp L6
+L5:
+L6:
+L3:
